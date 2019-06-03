24593b50afee8f46ccd4ad23129340bd9786d02d
GSteinberg <gppsteinberg@gmail.com>
2019-06-03 13:50:53 -0400

    Added dynamic data in templates

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+	return render(request, 'blog/post_list.html', {'posts':posts})
